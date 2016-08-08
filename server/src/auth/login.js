a1ffae0cfe087928c99f8e1ddd8bc035cab9b41e
Tim Ermilov <yamalight@gmail.com>
2016-08-08 18:55:50 +0200

    Validate user object after login

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -2,6 +2,10 @@ import passport from 'passport';
 
 export default (app) => {
   app.post('/api/login', passport.authenticate('local'), (req, res) => {
-    res.send(req.user);
+    if (req.user) {
+      res.send({user: req.user});
+    } else {
+      res.status(401).send({error: 'Error logging in!'});
+    }
   });
 };
