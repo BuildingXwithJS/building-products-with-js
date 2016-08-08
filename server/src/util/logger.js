8ed5504ef69d89a3681ca45c604a75b262eb9ba4
Tim Ermilov <yamalight@gmail.com>
2016-08-08 18:30:47 +0200

    Only log errors during testing

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -3,7 +3,17 @@ import winston from 'winston';
 export const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
-      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      level: do {
+        // TODO: remove eslint disable lines once the bug is fixed
+        // bugref: https://github.com/babel/eslint-plugin-babel/issues/13
+        if (process.env.NODE_ENV === 'testing') {
+          'error'; // eslint-disable-line
+        } else if (process.env.NODE_ENV === 'production') {
+          'info'; // eslint-disable-line
+        } else {
+          'debug'; // eslint-disable-line
+        }
+      },
       colorize: true,
       timestamp: true,
       prettyPrint: true,
