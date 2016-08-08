080a6b3d8785742df44de72175251425de120475
Tim Ermilov <yamalight@gmail.com>
2016-08-08 17:51:33 +0200

    Add login route and logic

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -1,5 +1,7 @@
-export default (app) => {
-  app.post('/api/login', (req, res) => {
+import passport from 'passport';
 
+export default (app) => {
+  app.post('/api/login', passport.authenticate('local'), (req, res) => {
+    res.send(req.user);
   });
 };
