1c5ff78fec62d40ecc6d69d7431ec49329961026
Tim Ermilov <yamalight@gmail.com>
2016-09-17 16:47:42 +0200

    Add debug serve script and use it for npm start

---
+++ client/serve.js
@@ -0,0 +1,52 @@
+/* eslint no-console: 0 */
+// start webpack
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+
+// create express
+const app = express();
+
+// setup hot reload
+config.plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  // setup no erros plugin
+  new webpack.NoErrorsPlugin(),
+];
+// override entry for hotload
+config.entry = [
+  'webpack-hot-middleware/client',
+  config.entry,
+];
+// returns a Compiler instance
+const compiler = webpack(config);
+// stats output config
+const statsConf = {
+  colors: true,
+  hash: false,
+  timings: true,
+  chunks: false,
+  chunkModules: false,
+  modules: false,
+};
+app.use(webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: statsConf,
+}));
+app.use(webpackHotMiddleware(compiler));
+
+// serve statics
+app.use(express.static(__dirname));
+// serve index
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// start server
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port 3000');
+});
