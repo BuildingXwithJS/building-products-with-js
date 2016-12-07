4fc346d6da1390d15771bb4073001597007f7f75
Javier Pérez <surtich@gmail.com>
2016-12-07 23:43:42 +0100

    [client][pages][home] move fetchQuestions from render to componentWillMount

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,7 +1,5 @@
 // npm packages
-import React from 'react';
-import _ from 'lodash';
-import {Link} from 'react-router';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import MediaQuery from 'react-responsive';
 
@@ -9,32 +7,39 @@ import MediaQuery from 'react-responsive';
 import {getAllQuestions, answerQuestion} from '../../store/actions';
 import {QuestionList, QuestionSingle} from '../../components/question';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   questions: state.questions.questions,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchQuestions: _.once(() => dispatch(getAllQuestions())),
+const mapDispatchToProps = dispatch => ({
+  fetchQuestions: () => dispatch(getAllQuestions()),
   doAnswer: payload => dispatch(answerQuestion(payload)),
 });
 
 
-const Home = ({fetchQuestions, doAnswer, questions}) => {
-  fetchQuestions();
+class Home extends Component {
 
-  return (
-    <div className="container">
-      <MediaQuery query="(min-width: 992px)">
-        {(matches) => {
-          if (matches) {
-            return <QuestionList questions={questions} doAnswer={doAnswer} />;
-          } else {
-            return <QuestionSingle questions={questions} doAnswer={doAnswer} />;
-          }
-        }}
-      </MediaQuery>
-    </div>
-  );
-};
+  componentWillMount() {
+    this.props.fetchQuestions();
+  }
+
+  render() {
+    const {doAnswer, questions} = this.props;
+
+    return (
+      <div className="container">
+        <MediaQuery query="(min-width: 992px)">
+          {(matches) => {
+            if (matches) {
+              return <QuestionList questions={questions} doAnswer={doAnswer} />;
+            } else {
+              return <QuestionSingle questions={questions} doAnswer={doAnswer} />;
+            }
+          }}
+        </MediaQuery>
+      </div>
+    );
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
