8cd8734b9ae82851fa1eef589ee21eea451b8b4b
Javier Pérez <surtich@gmail.com>
2016-12-07 23:18:50 +0100

    [client] Add a botton for collapse the question answers

--- client/src/components/question/question.js
+++ client/src/components/question/question.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default ({question, doAnswer}) => {
-  let answerInput;
+class Question extends Component {
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    doAnswer({question, answer: answerInput.value});
-    answerInput.value = '';
-    return false;
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: true,
+    };
+  }
 
-  return (
-    <div className="panel panel-default">
-      <div className="panel-heading">{question.text}</div>
-      <div className="panel-body">
-        {question.answers.length > 0 ? (
-          <ul className="list-group">
-            {question.answers.map((answer, i) => (
-              <li className="list-group-item" key={i}>{answer.answer}</li>
-            ))}
-          </ul>
-        ) : 'No answers yet'}
-      </div>
-      <div className="panel-footer">
-        <form className="form-horizontal">
-          <div className="input-group">
+  render() {
+    const {question, doAnswer} = this.props;
+    const {collapse} = this.state;
+    let answerInput;
+
+    const handleAnswerClick = (e) => {
+      e.preventDefault();
+      doAnswer({question, answer: answerInput.value});
+      answerInput.value = '';
+      return false;
+    };
+
+    const handleCollapseClick = (e) => {
+      e.preventDefault();
+      this.setState({
+        collapse: !this.state.collapse,
+      });
+      return false;
+    };
+
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <span className={`glyphicon glyphicon-${collapse ? 'plus' : 'minus'}`}
+            style={{cursor: 'pointer'}}
+            onClick={handleCollapseClick} />{' '}
+          {question.text}
+        </div>
+        {collapse ? null :
+        <div className="panel-body">
+          {question.answers.length > 0 ? (
+            <ul className="list-group">
+              {question.answers.map((answer, i) => (
+                <li className="list-group-item" key={i}>{answer.answer}</li>
+              ))}
+            </ul>
+          ) : 'No answers yet'}
+        </div>
+        }
+        {collapse ? null :
+        <div className="panel-footer">
+          <form className="form-horizontal">
+            <div className="input-group">
               <input
                 type="text"
                 className="form-control"
@@ -33,13 +60,16 @@ export default ({question, doAnswer}) => {
                 ref={(i) => { answerInput = i; }}
               />
               <span className="input-group-btn">
-                <button type="submit" className="btn btn-default" onClick={handleClick}>
+                <button type="submit" className="btn btn-default" onClick={handleAnswerClick}>
                   Answer
                 </button>
               </span>
-          </div>
-        </form>
+            </div>
+          </form>
+        </div>
+      }
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+export default Question;
