d1798c20b4d904eca4e3c1ae5dcc24c37b5e9f17
Anton Zdanov <akumael@gmail.com>
2018-04-03 18:52:26 +0300

    chore: fix typo

--- server/src/util/asyncRequest.js
+++ server/src/util/asyncRequest.js
@@ -3,6 +3,6 @@ import {logger} from './logger';
 export const asyncRequest = handler =>
   (req, res) =>
     handler(req, res).catch((e) => {
-      logger.debug('Error duing request:', e);
+      logger.debug('Error during request:', e);
       res.status(400).send({error: e.toString()});
     });
