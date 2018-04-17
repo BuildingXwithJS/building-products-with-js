afd5de1bdca5c6b987454a26dc5dc73a5a107b8d
iskilled <iskilled@gmail.com>
2018-04-17 20:02:19 +0100

    Fix notifications reducer tests

--- client/src/store/reducers/__tests__/notifications.test.js
+++ client/src/store/reducers/__tests__/notifications.test.js
@@ -6,24 +6,55 @@ test('# notifications reducer - add notification', () => {
   const testState = [];
   const action = {
     type: ActionTypes.ADD_NOTIFICATION,
-    payload: 'test',
+    payload: {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
   };
 
   expect(notifications(testState, action).length).toBe(1);
-  expect(notifications(testState, action)[0]).toBe('test');
+  expect(notifications(testState, action)[0]).toBe(action.payload);
+  expect(notifications(testState, action)).toEqual([...testState, action.payload]);
 });
 
 test('# notifications reducer - remove notification', () => {
-  const testState = ['test'];
+  const testState = [
+    {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
+    {
+      id: 2,
+      text: 'Another test notification!',
+      alertType: 'info',
+    }
+  ];
   const action = {
     type: ActionTypes.REMOVE_NOTIFICATION,
-    payload: 'test',
+    payload: {
+      notificationId: 1,
+    },
   };
 
-  expect(notifications(testState, action).length).toBe(0);
+  expect(notifications(testState, action)).toEqual([testState[1]]);
+  expect(notifications(testState, action).length).toBe(1);
 });
 
 test('# notifications reducer - errors', () => {
-  const testState = [];
-  expect(notifications(testState, {type: '-1'})).toEqual(testState);
+  const testState = [
+    {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
+    {
+      id: 2,
+      text: 'Another test notification!',
+      alertType: 'info',
+    }
+  ];
+
+  expect(notifications(testState, {type: -1})).toEqual(testState);
 });
